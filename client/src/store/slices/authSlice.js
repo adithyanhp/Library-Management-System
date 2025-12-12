4813c74aabdf7af00ea72bafef7d4d70c5b23aca
adithyanhp <adithyanhpm@gmail.com>
2025-12-12 23:26:18 +0530

    Updated authSlice.js file in client

--- client/src/store/slices/authSlice.js
+++ client/src/store/slices/authSlice.js
@@ -1,2 +1,13 @@
 import {createSlice} from "@reduxjs/toolkit";
 import axios from "axios";
+
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        loading: false,
+        error: null,
+        message: null
+        user: null
+    }
+})
