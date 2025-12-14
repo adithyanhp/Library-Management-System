45c15cd46e22fc93324cedd984f3cef845e967d0
adithyanhp <adithyanhpm@gmail.com>
2025-12-14 23:03:03 +0530

    Updated client files (autheSlice.js)

--- client/src/store/slices/authSlice.js
+++ client/src/store/slices/authSlice.js
@@ -12,18 +12,35 @@ const authSlice = createSlice({
         isAuthenticated: false,
     },
     reducers: {
-        registerRequest(state){
+        otpVerificationRequest(state){
             state.loading = true;
             state.error = null;
             state.message = null; 
         },
-        registerSuccess(state, action){
+        otpVerificationSuccess(state, action){
             state.loading = false;
             state.message = action.payload.message;
+            state.isAuthenticated = true;
+            state.user = action.payload.user;
         },
-        registerFailed(state, action){
+        otpVerificationFailed(state, action){
             state.loading = false;
             state.error = action.payload;
         },
     },
 });
+
+
+export const otpVerification = (email, otp) => async(dispatch) =>{
+    dispatch(authSlice.actions.otpVerificationRequest());
+    await axios.post("", {email, otp}, {
+        withCredentials: true,
+        headers: {
+            "Content-Type": "application/json",
+        }
+    }).then(res => {
+        dispatch(authSlice.actions.otpVerificationSuccess(res.data));
+    }).catch(err => {
+        authSlice.actions.otpVerificationFailed(err.response.data.message);
+    });
+};
